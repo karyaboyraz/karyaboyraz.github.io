0de767a0e529890c6b22eba7c360ede29ac2dc22
Karya <76717341+karyaboyraz@users.noreply.github.com>
2022-04-12 12:45:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('.counter').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
+
+// Aos
+
+AOS.init();
+
+// back to top
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
